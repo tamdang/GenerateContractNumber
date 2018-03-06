6261895a2e84ff022e0797290db5ef2b729c61d9
Dang Thanh Tam <dangtam@gmail.com>
2018-03-06 08:55:11 +0700

    do not end the pool

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/', function(req, res){
     pool.query('SELECT id, client, account_manager from contract', (err, data) => {
       console.log(err, data.rows)
       res.json({"message": data.rows});
-      pool.end()
+      // pool.end()
     })
 });
 
