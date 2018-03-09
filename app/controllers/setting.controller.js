79c332dc44644abd860c24614e54830d36130758
Dang Thanh Tam <dangtam@gmail.com>
2018-03-10 05:51:27 +0700

    Refactor setting to use methods in the util Drop using the EXPERIMENTAL with the callee and caller. Caller needs to specify the name of the model by passing the json object

--- app/controllers/setting.controller.js
+++ app/controllers/setting.controller.js
@@ -1,43 +1,18 @@
 var Setting = require('../models/setting.model.js')
-const {
-  RECORD_LIMIT
-} = require('../../setting/contants')
 
-exports.create = function(req, res){
-  const {key, value} = req.body
-  var setting = new Setting({key, value})
-    setting.save(function(err, setting) {
-    if(err) {
-      res.status(500).send({message: "Some error occurred while creating the setting."})
-      return
-    }
-    res.send(setting)
-  })
-}
-
-exports.update = function(req, res){
-  const {id} = req.params
-  const {key, value} = req.body
-  Setting.findByIdAndUpdate(id,{key,value},function(err,setting){
-    if(err) {
-      res.status(500).send({message: "Could not update a setting with id " + id})
-      return
-    }
-    res.send(setting)
-  })
-}
-
-exports.deleteById = require('./util.controller').deleteById(Setting)
-exports.getById = require('./util.controller').getById(Setting)
-exports.getAll = require('./util.controller').getAll(Setting)
+exports.create = require('./util.controller').create({Setting})
+exports.update = require('./util.controller').update({Setting})
+exports.deleteById = require('./util.controller').deleteById({Setting})
+exports.getById = require('./util.controller').getById({Setting})
+exports.getAll = require('./util.controller').getAll({Setting})
 
 exports.getByKey = function(req, res){
   const {key} = req.params
   Setting.findOne({'key':key}).exec(function(err, setting){
-    if(err) {
+    if(err || !setting) {
       res.status(500).send({message: "Could not retrieve setting"})
       return
-    } 
+    }
     res.send({'value':setting['value']})
   })
 }
--- app/controllers/util.controller.js
+++ app/controllers/util.controller.js
@@ -1,33 +1,25 @@
-exports.getById = getById
-exports.deleteById = deleteById
-exports.getAll = getAll
-
 const {
   RECORD_LIMIT
 } = require('../../setting/contants')
 
-const getNameFromFound = (found) => found[0].split('\(')[1].trimLeft()
-
-function getById(model){
-  var re = /getById\(\s*\w+/
-  console.log('getById', arguments.callee.caller)
-  let modelName = getNameFromFound(arguments.callee.caller.toString().match(re))
-
+exports.getById = function(modelObject){
+  const modelName = Object.keys(modelObject)[0]
+  const model = modelObject[modelName]
   return function(req, res){
     const {id} = req.params
-    model.findById(id,function(err, setting){
+    model.findById(id,function(err, data){
       if(err) {
         res.status(500).send({message: "Could not retrieve " + modelName})
         return
       } 
-      res.send({[modelName]: setting})
+      res.send(data)
     })
   }
 }
 
-function deleteById(model){
-  var re = /deleteById\(\s*\w+/
-  let modelName = getNameFromFound(arguments.callee.caller.toString().match(re))
+exports.deleteById = function(modelObject){
+  const modelName = Object.keys(modelObject)[0]
+  const model = modelObject[modelName]
   return function(req, res){
     const {id} = req.params
     model.remove({_id: id}, function(err, data) {
@@ -41,16 +33,55 @@ function deleteById(model){
   }
 }
 
-function getAll(model){
-  var re = /getAll\(\s*\w+/
-  let modelName = getNameFromFound(arguments.callee.caller.toString().match(re))
+exports.getAll = function(modelObject){
+  const modelName = Object.keys(modelObject)[0]
+  const model = modelObject[modelName]
   return function (req, res) {
-    model.find().limit(RECORD_LIMIT).exec(function(err, models){
+    model.find().limit(RECORD_LIMIT).exec(function(err, data){
       if(err) {
         res.status(500).send({message: "Could not retrieve "+modelName})
         return
       } 
-      res.send(models)
+      res.send(data)
+    })
+  }
+}
+
+exports.update = function(modelObject){
+  //modelObject is the json with name of the model and the model as the value of the name
+  //This is needed in order to display the message to user because we don't know 
+  //how to get the model name
+  const modelName = Object.keys(modelObject)[0]
+  const model = modelObject[modelName]
+  return function(req, res){
+    const {id} = req.params
+    model.findByIdAndUpdate(id,req.body,function(err,data){
+      if(err) {
+        res.status(500).send({message: "Could not update a " + modelName + " with id " + id})
+        return
+      }
+      model.findById(id,(err,data)=>{
+        if(err) {
+          res.status(500).send({message: "Could not update a " + modelName + " with id " + id})
+          return
+        }
+        res.send(data)
+      })
+    })
+  }
+}
+
+exports.create = function(modelObject){
+  const modelName = Object.keys(modelObject)[0]
+  const model = modelObject[modelName]
+  return function(req, res){
+    var newModel = new model(req.body)
+      newModel.save(function(err, data) {
+      if(err) {
+        res.status(500).send({message: "Some error occurred while creating the "+modelName})
+        return
+      }
+      res.send(data)
     })
   }
 }
