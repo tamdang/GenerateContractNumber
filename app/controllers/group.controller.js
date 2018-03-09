0d8f5b1b7d3ecab3b7d779d8e21b5f160f440291
Dang Thanh Tam <dangtam@gmail.com>
2018-03-10 06:03:29 +0700

    Use the utilCRUD for the group controller

--- app/controllers/group.controller.js
+++ app/controllers/group.controller.js
@@ -2,44 +2,16 @@ var Group = require('../models/group.model.js')
 var GroupUser = require('../models/group_user.model')
 var User = require('../models/user.model')
 var mongoose = require('mongoose')
+var utilCRUD = require('./util.controller')
 const {
   RECORD_LIMIT
 } = require('../../setting/contants')
 
-exports.create = function(req, res){
-  const {name, amUserId} = req.body
-
-  var group = new Group({name, amUserId})
-    group.save(function(err, group) {
-    if(err) {
-      res.status(500).send({message: "Some error occurred while creating the group."})
-      return
-    }
-    res.send(group)
-  })
-}
-
-exports.update = function(req, res){
-  const {id} = req.params
-  Group.findById(id, function(err, group) {
-    if(err) {
-      res.status(500).send({message: "Could not find a group with id " + id})
-      return
-    }
-
-    Object.keys(req.body).forEach(k => {
-      group[k] = req.body[k]
-    })
-
-    group.save(function(err, group){
-      if(err) {
-        res.status(500).send({message: "Could not update group with id " + id})
-        return
-      } 
-      res.send(group)
-    })
-  })
-}
+exports.create = utilCRUD.create({Group})
+exports.update = utilCRUD.update({Group})
+exports.delete = utilCRUD.deleteById({Group})
+exports.getAll = utilCRUD.getAll({Group})
+exports.getById = utilCRUD.getById({Group})
 
 exports.addUser = function(req, res){
   const {userId} = req.body
@@ -63,39 +35,6 @@ exports.removeUser = function(req, res){
       return
     }
     res.send({message: "User was removed from group successfully!"})
-  })
-}
-
-exports.delete = function(req, res){
-  const {id} = req.params
-  Group.remove({_id: id}, function(err, data) {
-    if(err) {
-      res.status(500).send({message: "Could not delete group with id " + id});
-      return
-    } else {
-      res.send({message: "Group was deleted successfully!"})
-    }
-  })
-}
-
-exports.getById = function(req, res){
-  const {id} = req.params
-  Group.findById(id,function(err,group){
-    if(err) {
-      res.status(500).send({message: "Could not retrieve group"})
-      return
-    } 
-    res.send(group)
-  })
-}
-
-exports.getAll = function(req, res) {
-  Group.find().limit(RECORD_LIMIT).exec(function(err, groups){
-    if(err) {
-      res.status(500).send({message: "Could not retrieve group"})
-      return
-    } 
-    res.send(groups)
   })
 }
 
