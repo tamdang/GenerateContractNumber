376259ff9b8605521a86847d354dee4069f40fbc
Dang Thanh Tam <dangtam@gmail.com>
2019-12-29 06:39:59 +0700

    try to fix the access control header politc

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
 })
 
