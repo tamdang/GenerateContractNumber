b421f06d41db206ff9e31ec414524d31a8a77c5d
Dang Thanh Tam <dangtam@gmail.com>
2019-12-28 23:49:01 +0700

    fix the bug that the app is crashed

--- app/routes/maggi.routes.js
+++ app/routes/maggi.routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
   var maggi = require('../controllers/maggi.controller.js')
-  app.post('/api/maggi', user.create)
-  app.get('/api/maggi',user.getAll)
+  app.post('/api/maggi', maggi.create)
+  app.get('/api/maggi',maggi.getAll)
 }
--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://tamdang:mat891432@localhost:5432/myapp'
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:32769/contractms'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://heroku_n6hzqvbw:gqqtbld6v4ihic8nhp3bsg50c0@ds257808.mlab.com:57808/heroku_n6hzqvbw'
 console.log(MONGODB_URI)
 module.exports = {
   url: MONGODB_URI
