ba0dc7d60c5a0093eb523673fe1956fbdf23b824
Dang Thanh Tam <dangtam@gmail.com>
2019-12-30 20:00:47 +0700

    change the question to number in order to store 3 different values: 0 --> no answer 1 --> answer and preferCrush/Cooking/Traditional food 2 --> answer and dot not preferCrush/Cooking/Traditional food

--- app/models/maggi.model.js
+++ app/models/maggi.model.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var MaggiSchema = mongoose.Schema({
   name: String,
   birthDate: Date,
-  changeCrush: Boolean,
-  preferCooking: Boolean,
-  preferTraditionalFood: Boolean
+  changeCrush: Number, //0 --> no value selected, 1 --> ok to change, 2 --> not ok to change
+  preferCooking: Number, //0 --> no value selected, 1 --> prefer cooking, 2 --> not prefer cooking
+  preferTraditionalFood: Number //0 --> no value selected, 1 --> prefer traditional food, 2 --> not prefer traditional food
 })
 
 module.exports = mongoose.model('Maggi', MaggiSchema)
